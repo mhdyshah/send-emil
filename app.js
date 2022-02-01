9c21ced6c5a281035506d51df8e49a30b65c1e62
mhdyshah <mahdi.shahsavari79@gmail.com>
2022-02-01 15:47:13 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const app = express();
+const route = express.Router();
+const port = process.env.PORT || 5000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use('/v1', route);
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    },
+    secure: true, // upgrades later with STARTTLS -- change this based on the PORT
+});
+
+route.post('/text-mail', (req, res) => {
+    const {
+        to,
+        subject,
+        text
+    } = req.body;
+    const mailData = {
+        from: process.env.EMAIL,
+        to: to,
+        subject: subject,
+        text: text,
+        html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
+    };
+
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        res.status(200).send({
+            message: "Mail send",
+            message_id: info.messageId
+        });
+    });
+});
+
+
+route.post('/attachments-mail', (req, res) => {
+    const {
+        to,
+        subject,
+        text
+    } = req.body;
+    const mailData = {
+        from: process.env.EMAIL,
+        to: to,
+        subject: subject,
+        text: text,
+        html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
+        attachments: [{ // file on disk as an attachment
+                filename: 'linux.jpg',
+                path: 'linux.jpg'
+            },
+            { // file on disk as an attachment
+                filename: 'text_file.txt',
+                path: 'text_file.txt'
+            }
+        ]
+    };
+
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        res.status(200).send({
+            message: "Mail send",
+            message_id: info.messageId
+        });
+    });
+});
